f190fc890d61e772a54184077fa9b17b5f07eb52
abdelrhmanehab10 <abdelrhmanehab047@gmail.com>
2025-07-02 16:24:01 +0300

    feat: integrate uploadthing for file uploads and add sonner for notifications
    - Added `next-themes` and `sonner` dependencies for theme management and notifications.
    - Implemented file upload functionality using `uploadthing` with a new `UploadFileInput` component.
    - Introduced a custom `Toaster` component for displaying notifications.
    - Created API routes for handling file uploads.

--- components/upload-file-input.tsx
+++ components/upload-file-input.tsx
@@ -1,55 +1,56 @@
 "use client";
 
-import {
-  AlertCircleIcon,
-  PaperclipIcon,
-  UploadIcon,
-  XIcon,
-} from "lucide-react";
+import { Loader2, PaperclipIcon, UploadIcon, XIcon } from "lucide-react";
 
-import { formatBytes, useFileUpload } from "@/hooks/use-file-upload";
 import { Button } from "@/components/ui/button";
+import { useDropzone } from "@uploadthing/react";
+import { useUploadThing } from "@/utils/uploadthing";
+import { useCallback, useState } from "react";
+import {
+  generateClientDropzoneAccept,
+  generatePermittedFileTypes,
+} from "uploadthing/client";
+import { toast } from "sonner";
 
 export default function UploadFileInput() {
-  const maxSize = 10 * 1024 * 1024;
+  const [files, setFiles] = useState<File[]>([]);
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    setFiles(acceptedFiles);
+  }, []);
 
-  const [
-    { files, isDragging, errors },
+  const { startUpload, routeConfig, isUploading } = useUploadThing(
+    "resumeUploader",
     {
-      handleDragEnter,
-      handleDragLeave,
-      handleDragOver,
-      handleDrop,
-      openFileDialog,
-      removeFile,
-      getInputProps,
-    },
-  ] = useFileUpload({
-    maxSize,
-    accept:
-      ".pdf,.docx,application/pdf,application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-  });
+      onClientUploadComplete: () => {
+        toast.success("Uploaded Successfully!");
+        setFiles([]);
+      },
+      onUploadError: (e) => {
+        console.log("ERROR_UPLOAD_FILE: ", e.message);
+        toast.error("Error occurred while uploading");
+      },
+    }
+  );
 
-  const file = files[0];
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: generateClientDropzoneAccept(
+      generatePermittedFileTypes(routeConfig).fileTypes
+    ),
+  });
 
   return (
     <div className="flex flex-col gap-2">
       <div
         role="button"
-        onClick={openFileDialog}
-        onDragEnter={handleDragEnter}
-        onDragLeave={handleDragLeave}
-        onDragOver={handleDragOver}
-        onDrop={handleDrop}
-        data-dragging={isDragging || undefined}
         className="border-input hover:bg-accent/50 data-[dragging=true]:bg-accent/50 has-[input:focus]:border-ring has-[input:focus]:ring-ring/50 flex min-h-40 flex-col items-center justify-center rounded-xl border border-dashed p-4 transition-colors has-disabled:pointer-events-none has-disabled:opacity-50 has-[input:focus]:ring-[3px]"
+        {...getRootProps()}
       >
         <input
-          {...getInputProps()}
           className="sr-only"
           aria-label="Upload file"
-          accept=".pdf, .docx"
-          disabled={Boolean(file)}
+          disabled={Boolean(files[0])}
+          {...getInputProps()}
         />
 
         <div className="flex flex-col items-center justify-center text-center">
@@ -61,65 +62,131 @@ export default function UploadFileInput() {
           </div>
           <p className="mb-1.5 text-sm font-medium">Upload Your Resume</p>
           <p className="text-muted-foreground text-xs">
-            Drag & drop PDF or DOCX files or click to browse (max.{" "}
-            {formatBytes(maxSize)})
+            Drag & drop PDF or DOCX files or click to browse (max. 4MB)
           </p>
         </div>
       </div>
-
-      {errors.length > 0 && (
-        <div
-          className="text-destructive flex items-center gap-1 text-xs"
-          role="alert"
-        >
-          <AlertCircleIcon className="size-3 shrink-0" />
-          <span>{errors[0]}</span>
-        </div>
-      )}
-
-      {file && (
-        <div className="space-y-2">
-          <div
-            key={file.id}
-            className="flex items-center justify-between gap-2 rounded-xl border px-4 py-2"
-          >
-            <div className="flex items-center gap-3 overflow-hidden">
-              <PaperclipIcon
-                className="size-4 shrink-0 opacity-60"
-                aria-hidden="true"
-              />
-              <div className="min-w-0">
-                <p className="truncate text-[13px] font-medium">
-                  {file.file.name}
-                </p>
-              </div>
-            </div>
-
-            <Button
-              size="icon"
-              variant="ghost"
-              className="text-muted-foreground/80 hover:text-foreground -me-2 size-8 hover:bg-transparent"
-              onClick={() => removeFile(files[0]?.id)}
-              aria-label="Remove file"
+      <div>
+        {files.length > 0 && (
+          <div className="space-y-2">
+            <div
+              key={files[0].name}
+              className="flex items-center justify-between gap-2 rounded-xl border px-4 py-2"
             >
-              <XIcon className="size-4" aria-hidden="true" />
-            </Button>
+              <div className="flex items-center gap-3 overflow-hidden">
+                <PaperclipIcon
+                  className="size-4 shrink-0 opacity-60"
+                  aria-hidden="true"
+                />
+                <div className="min-w-0">
+                  <p className="truncate text-[13px] font-medium">
+                    {files[0].name}
+                  </p>
+                </div>
+              </div>
+
+              <Button
+                size="icon"
+                variant="ghost"
+                className="text-muted-foreground/80 hover:text-foreground -me-2 size-8 hover:bg-transparent"
+                onClick={() => setFiles([])}
+                aria-label="Remove file"
+              >
+                <XIcon className="size-4" aria-hidden="true" />
+              </Button>
+            </div>
           </div>
-        </div>
-      )}
-
-      <Button className="mt-2 w-full" disabled={!file}>
-        Analyze ✨
+        )}
+      </div>
+      <Button
+        className="w-full"
+        disabled={!files[0]}
+        onClick={() => startUpload(files)}
+      >
+        {isUploading ? <Loader2 className="animate-spin" /> : "Analyze ✨"}
       </Button>
-
       <p
         aria-live="polite"
         role="region"
-        className="text-muted-foreground mt-1 text-center text-xs"
+        className="text-muted-foreground text-center text-xs"
       >
         Upload your resume, then click &apos;Analyze&apos; or press Enter to
         begin the analysis.
       </p>
     </div>
+
+    // <div {...getRootProps()} className="flex flex-col gap-2">
+    //   <div
+    //     role="button"
+    //     className="border-input hover:bg-accent/50 data-[dragging=true]:bg-accent/50 has-[input:focus]:border-ring has-[input:focus]:ring-ring/50 flex min-h-40 flex-col items-center justify-center rounded-xl border border-dashed p-4 transition-colors has-disabled:pointer-events-none has-disabled:opacity-50 has-[input:focus]:ring-[3px]"
+    //   >
+    //     <input
+    //       {...getInputProps()}
+    //       className="sr-only"
+    //       aria-label="Upload file"
+    //     />
+
+    //     <div className="flex flex-col items-center justify-center text-center">
+    //       <div
+    //         className="bg-background mb-2 flex size-11 shrink-0 items-center justify-center rounded-full border"
+    //         aria-hidden="true"
+    //       >
+    //         <UploadIcon className="size-4 opacity-60" />
+    //       </div>
+    //       <p className="mb-1.5 text-sm font-medium">Upload Your Resume</p>
+    //       <p className="text-muted-foreground text-xs">
+    //         Drag & drop PDF or DOCX files or click to browse (max. 4MB)
+    //       </p>
+    //     </div>
+    //   </div>
+
+    //   {files.length > 0 && (
+    //     <div className="space-y-2">
+    //       <div
+    //         key={files[0].name}
+    //         className="flex items-center justify-between gap-2 rounded-xl border px-4 py-2"
+    //       >
+    //         <div className="flex items-center gap-3 overflow-hidden">
+    //           <PaperclipIcon
+    //             className="size-4 shrink-0 opacity-60"
+    //             aria-hidden="true"
+    //           />
+    //           <div className="min-w-0">
+    //             <p className="truncate text-[13px] font-medium">
+    //               {files[0].name}
+    //             </p>
+    //           </div>
+    //         </div>
+
+    //         <Button
+    //           size="icon"
+    //           variant="ghost"
+    //           className="text-muted-foreground/80 hover:text-foreground -me-2 size-8 hover:bg-transparent"
+    //           onClick={() => setFiles([])}
+    //           aria-label="Remove file"
+    //         >
+    //           <XIcon className="size-4" aria-hidden="true" />
+    //         </Button>
+    //       </div>
+    //     </div>
+    //   )}
+
+    //   <Button
+    //     className="mt-2 w-full"
+    //     disabled={!files[0]}
+    //     onClick={() => startUpload(files)}
+    //   >
+    //     Analyze ✨
+    //   </Button>
+
+    //   <p
+    //     aria-live="polite"
+    //     role="region"
+    //     className="text-muted-foreground mt-1 text-center text-xs"
+    //   >
+    //     Upload your resume, then click &apos;Analyze&apos; or press Enter to
+    //     begin the analysis.
+    //   </p>
+    // </div>
   );
 }
