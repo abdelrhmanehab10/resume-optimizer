cf6782f5d85a2bcbcdd8a6406d5f2be46e035f54
abdelrhmanehab10 <abdelrhmanehab047@gmail.com>
2025-07-02 18:35:17 +0300

    refactor: simplify file upload router by removing unused code and comments
    - Cleaned up the `ourFileRouter` by removing commented-out code and unnecessary sections.
    - Streamlined the `onUploadComplete` function to focus on logging the uploaded file URL.

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,54 +1,16 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
-import { UploadThingError } from "uploadthing/server";
 
 const f = createUploadthing();
 
-// Fake auth function
-// const auth = (req: Request) => ({ id: "fakeId" });
-
-// FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
   resumeUploader: f({
     pdf: {
       maxFileCount: 1,
       maxFileSize: "4MB",
     },
-    // image: {
-    /**
-     * For full list of options and defaults, see the File Route API reference
-     * @see https://docs.uploadthing.com/file-routes#route-config
-     */
-
-    //   maxFileSize: "4MB",
-    //   maxFileCount: 1,
-    // },
-  })
-    // Set permissions and file types for this FileRoute
-    // .middleware(async ({ req }) => {
-    //   // This code runs on your server before upload
-    //   const user = await auth(req);
-
-    //   // If you throw, the user will not be able to upload
-    //   if (!user) throw new UploadThingError("Unauthorized");
-
-    //   // Whatever is returned here is accessible in onUploadComplete as `metadata`
-    //   return { userId: user.id };
-    // })
-    .onUploadComplete(
-      async ({
-        // metadata,
-        file,
-      }) => {
-        // This code RUNS ON YOUR SERVER after upload
-        // console.log("Upload complete for userId:", metadata.userId);
-
-        console.log("file url", file.ufsUrl);
-
-        // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-        // return { uploadedBy: metadata.userId };
-      }
-    ),
+  }).onUploadComplete(async ({ file }) => {
+    console.log("file url", file.ufsUrl);
+  }),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
