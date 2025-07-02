4c4f6db2639872a7e0309fa9bc10c6c41c56de41
abdelrhmanehab10 <abdelrhmanehab047@gmail.com>
2025-07-02 18:31:18 +0300

    fix: adjust padding on home page main container

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import UploadFileInput from "@/components/upload-file-input";
 
 export default function Home() {
   return (
-    <main className="flex h-screen w-screen items-center justify-center">
+    <main className="flex h-screen w-screen items-center justify-center px-3 md:px-0">
       <div className="w-fit">
         <UploadFileInput />
       </div>
